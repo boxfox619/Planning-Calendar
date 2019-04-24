ed89b6476a8ff2cace0002b23d008fd37f0e7aa5
Boxfox <boxfoxsg619@gmail.com>
2019-04-24 13:21:40 +0900

    Calendar controller 테스트 수정

--- src/components/__tests__/CalendarController.test.tsx
+++ src/components/__tests__/CalendarController.test.tsx
@@ -27,7 +27,7 @@ describe('<CalendarController />', () => {
         buttons.at(0).simulate('click');
         buttons.at(1).simulate('click');
         expect(changeMoment.mock.calls.length).toBe(2);
-        expect(changeMoment.mock.calls[0][0].format()).toBe('2014-01-27T00:00:00.000Z');
-        expect(changeMoment.mock.calls[1][0].format()).toBe('2014-03-27T00:00:00.000Z');
+        expect(changeMoment.mock.calls[0][0].format()).toBe('2014-01-27T00:00:00Z');
+        expect(changeMoment.mock.calls[1][0].format()).toBe('2014-03-27T00:00:00Z');
     });
 });
